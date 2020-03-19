795b47a636cbdc53c538910597d3c8de4b9242d2
Santi <santi.gonzalez@wallapop.com>
2020-03-19 12:17:16 +0100

    Simplify method signature

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -13,16 +13,16 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     override fun getScore() = when {
             scoreAreEqual() -> mapEqualScore()
             anyScoreIsMoreThanFour() -> mapDifferenceScore("")
-            else -> mapScore(0, "")
+            else -> mapScore()
         }
 
     private fun anyScoreIsMoreThanFour() = m_score1 >= 4 || m_score2 >= 4
 
     private fun scoreAreEqual() = m_score1 == m_score2
 
-    private fun mapScore(tempScore: Int, score: String): String {
-        var tempScore1 = tempScore
-        var score1 = score
+    private fun mapScore(): String {
+        var tempScore1 = 0
+        var score1 = ""
         for (i in 1..2) {
             if (i == 1)
                 tempScore1 = m_score1
