77551b0ad812dedc64145e838fff5b530ccea603
Santi <santi.gonzalez@wallapop.com>
2020-03-19 12:28:32 +0100

    Rename player score properties

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,13 +1,13 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var player1Score: Int = 0
+    private var player2Score: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
-            m_score1 += 1
+            player1Score += 1
         else
-            m_score2 += 1
+            player2Score += 1
     }
 
     override fun getScore() = when {
@@ -16,11 +16,11 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
             else -> mapScore()
         }
 
-    private fun anyScoreIsMoreThanFour() = m_score1 >= 4 || m_score2 >= 4
+    private fun anyScoreIsMoreThanFour() = player1Score >= 4 || player2Score >= 4
 
-    private fun scoreAreEqual() = m_score1 == m_score2
+    private fun scoreAreEqual() = player1Score == player2Score
 
-    private fun mapScore(): String = addScoreName(m_score1) + "-" + addScoreName(m_score2)
+    private fun mapScore(): String = addScoreName(player1Score) + "-" + addScoreName(player2Score)
 
     private fun addScoreName(playerScore: Int): String = when (playerScore) {
         0 -> "Love"
@@ -32,7 +32,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
 
     private fun mapDifferenceScore(score: String): String {
         var score1 = score
-        val minusResult = m_score1 - m_score2
+        val minusResult = player1Score - player2Score
         score1 = when {
             minusResult == 1 -> "Advantage player1"
             minusResult == -1 -> "Advantage player2"
@@ -43,7 +43,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     }
 
     private fun mapEqualScore(): String {
-        return when (m_score1) {
+        return when (player1Score) {
             0 -> "Love-All"
             1 -> "Fifteen-All"
             2 -> "Thirty-All"
